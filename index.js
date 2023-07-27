fe19dd9192403bc5a9a5f02e93fd6e0cc0264d28
Md Rihwan Jannat <you@example.com>
2023-07-27 18:26:59 +0600

    5th number task-complete

--- index.js
+++ index.js
@@ -60,7 +60,6 @@ const output = findFrequentElement(inputArray);
 console.log(output);
 
 
-
 // Task 4: Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers.
 
 function towNumbers (nums, target) {
